ec6aa60c09f409bc9b493aca4516a0f5f987e461
Michael Wong <mik9080@gmail.com>
2017-10-09 21:02:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,7 +30,8 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board,index)
-  index.between?(0,8) #&& !position_taken(board,index)
+  #index.between?(0,8) && 
+  !position_taken(board,index)
 end
 
 def turn(board)
