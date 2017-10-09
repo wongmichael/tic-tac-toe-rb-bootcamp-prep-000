a3552e5f69eadf4a6a45c10db07995fa5bb59b8b
Michael Wong <mik9080@gmail.com>
2017-10-09 21:43:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,7 +71,10 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board)!=false && !won?(board).nil?
-    board[won?(board)[0]]
+  #if won?(board)!=false && !won?(board).nil?
+  #  board[won?(board).first]
+  #end
+  if won?(board).kind_of?(Array)
+    board[won?(board).first]
   end
 end
